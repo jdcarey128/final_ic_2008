fd71b72cd3bcdc1628f5137b855da61ad9a97e9c
Joshua Carey <jdcarey128@gmail.com>
2020-09-23 09:29:05 -0600

    Add initialize and add recipe for cook book

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -16,6 +16,8 @@ class CookBookTest < Minitest::Test
     cookbook = CookBook.new
     recipe1 = Recipe.new("Mac and Cheese")
     recipe2 = Recipe.new("Cheese Burger")
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
 
