5b1caa4872bf19de0f6e21b39262b6c79708cdd4
Joshua Carey <jdcarey128@gmail.com>
2020-09-23 09:44:14 -0600

    Add highest calorie meal method for cook book

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -14,7 +14,13 @@ class CookBook
       recipe.ingredients.map do |ingredient|
         ingredient.name
       end
-    end.uniq 
+    end.uniq
+  end
+
+  def highest_calorie_meal
+    @recipes.max_by do |recipe|
+      recipe.total_calories
+    end
   end
 
 end
